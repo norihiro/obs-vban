033967132487ad7c62b6154f8d243ee3575dca12
Norihiro Kamae <norihiro@nagater.net>
2024-10-28 19:07:44 +0900

    Separate API of resolving host name into multiple functions

--- src/resolve-thread.c
+++ src/resolve-thread.c
@@ -2,9 +2,9 @@
 
 #include <obs-module.h>
 #include <util/threading.h>
+#include "socket.h"
 #ifndef _WIN32
 #include <sys/types.h>
-#include <sys/socket.h>
 #include <netdb.h>
 #include <ctype.h>
 #else
@@ -19,7 +19,13 @@ static volatile long long n_resolving = 0;
 
 struct resolve_thread_s
 {
+	volatile long refcnt;
+
 	char *name;
+	struct in_addr addr;
+	bool succeeded;
+	volatile bool done;
+
 	resolve_thread_resolved_cb resolved_cb;
 	resolve_thread_failed_cb failed_cb;
 	void *cb_data;
@@ -57,24 +63,61 @@ static bool is_valid_hostname(const char *host)
 	return prev_was_alnum;
 }
 
-bool resolve_thread_start(const char *name, void *cb_data, resolve_thread_resolved_cb resolved_cb,
-			  resolve_thread_failed_cb failed_cb)
+resolve_thread_t *resolve_thread_create(const char *name)
 {
-	if (!is_valid_hostname(name)) {
-		if (failed_cb)
-			failed_cb(cb_data);
-		return false;
-	}
+	if (!is_valid_hostname(name))
+		return NULL;
 
 	pthread_mutex_lock(&mutex);
 	n_resolving++;
 	pthread_mutex_unlock(&mutex);
 
-	struct resolve_thread_s *ctx = bzalloc(sizeof(struct resolve_thread_s));
+	resolve_thread_t *ctx = bzalloc(sizeof(struct resolve_thread_s));
 	ctx->name = bstrdup(name);
+
+	return ctx;
+}
+
+static void resolve_thread_destroy(resolve_thread_t *ctx)
+{
+	bfree(ctx->name);
+	bfree(ctx);
+
+	pthread_mutex_lock(&mutex);
+	if (!--n_resolving)
+		pthread_cond_signal(&cond);
+	pthread_mutex_unlock(&mutex);
+}
+
+resolve_thread_t *resolve_thread_get_ref(resolve_thread_t *ctx)
+{
+	long owners = os_atomic_load_long(&ctx->refcnt);
+	while (owners > -1) {
+		if (os_atomic_compare_exchange_long(&ctx->refcnt, &owners, owners + 1))
+			return ctx;
+	}
+	return NULL;
+}
+
+void resolve_thread_release(resolve_thread_t *ctx)
+{
+	if (os_atomic_dec_long(&ctx->refcnt) == -1)
+		resolve_thread_destroy(ctx);
+}
+
+void resolve_thread_set_callbacks(resolve_thread_t *ctx, void *cb_data, resolve_thread_resolved_cb resolved_cb,
+				  resolve_thread_failed_cb failed_cb)
+{
 	ctx->resolved_cb = resolved_cb;
 	ctx->failed_cb = failed_cb;
 	ctx->cb_data = cb_data;
+}
+
+bool resolve_thread_start(resolve_thread_t *ctx)
+{
+	ctx = resolve_thread_get_ref(ctx);
+	if (!ctx)
+		return false;
 
 	pthread_t thread;
 	if (pthread_create(&thread, NULL, resolve_thread_main, ctx)) {
@@ -104,24 +147,43 @@ static void *resolve_thread_main(void *data)
 		struct in_addr addr = {0};
 		addr.s_addr = ((struct sockaddr_in *)res->ai_addr)->sin_addr.s_addr;
 		freeaddrinfo(res);
+		ctx->addr = addr;
 
 		if (ctx->resolved_cb)
 			ctx->resolved_cb(ctx->cb_data, &addr);
+
+		ctx->succeeded = true;
 	}
 	else {
 		if (ctx->failed_cb)
 			ctx->failed_cb(ctx->cb_data);
 	}
 
-	bfree(ctx->name);
-	bfree(ctx);
+	os_atomic_store_bool(&ctx->done, true);
 
-	pthread_mutex_lock(&mutex);
-	if (!--n_resolving)
-		pthread_cond_signal(&cond);
-	pthread_mutex_unlock(&mutex);
+	if (ctx->succeeded)
+		blog(LOG_DEBUG, "Succeeded to resolve '%s' to %s", ctx->name, inet_ntoa(ctx->addr));
+	else
+		blog(LOG_DEBUG, "Failed to resolve '%s'", ctx->name);
 
+	resolve_thread_release(ctx);
 	return NULL;
+}
+
+bool resolve_thread_done(const resolve_thread_t *ctx)
+{
+	return os_atomic_load_bool(&ctx->done);
+}
+
+bool resolve_thread_get_addr(const resolve_thread_t *ctx, struct in_addr *addr)
+{
+	if (os_atomic_load_bool(&ctx->done) && ctx->succeeded) {
+		*addr = ctx->addr;
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 void resolve_thread_wait_all()
--- src/resolve-thread.h
+++ src/resolve-thread.h
@@ -4,16 +4,80 @@
 extern "C" {
 #endif
 
-/* The API resolves host name in a newly created thread.
+/**
+ * The API resolves host name in a newly created thread.
  * If the resolution has succeeded, `resolved` will be called.
  * Otherwise, `failed` will be called.
  */
 
+struct in_addr;
+typedef struct resolve_thread_s resolve_thread_t;
+
 typedef void (*resolve_thread_resolved_cb)(void *data, const struct in_addr *addr);
 typedef void (*resolve_thread_failed_cb)(void *data);
 
-bool resolve_thread_start(const char *name, void *cb_data, resolve_thread_resolved_cb resolved_cb,
-			  resolve_thread_failed_cb failed_cb);
+/**
+ * Create a host name resolution thread.
+ * @param[in] name  Host name
+ * @return          Context to resolve the name.
+ *
+ * The returned context should be released by `resolve_thread_release`.
+ */
+resolve_thread_t *resolve_thread_create(const char *name);
+
+/**
+ * Increment the reference count.
+ * @param[in] ctx  The context.
+ * @return         Context if successfully increment the reference count, otherwise NULL.
+ */
+resolve_thread_t *resolve_thread_get_ref(resolve_thread_t *ctx);
+
+/**
+ * Release the context.
+ * @param[in] ctx  The context.
+ */
+void resolve_thread_release(resolve_thread_t *ctx);
+
+/**
+ * Set the callback functions.
+ * @param[in] ctx          The context.
+ * @param[in] cb_data      A parameter transparently passed to the callback functions.
+ * @param[in] resolved_cb  A function called when the resolution has succeeded.
+ * @param[in] failed_cb    A function called when the resolution has failed.
+ *
+ * Either the `resolved_cb` or `failed_cb` will be called only once.
+ */
+void resolve_thread_set_callbacks(resolve_thread_t *ctx, void *cb_data, resolve_thread_resolved_cb resolved_cb,
+				  resolve_thread_failed_cb failed_cb);
+
+/**
+ * Start the resolution.
+ * @param[in] ctx  The context.
+ *
+ * Inside this API, the thread will be created.
+ */
+bool resolve_thread_start(resolve_thread_t *ctx);
+
+/**
+ * Query if the resolution was done.
+ * @param[in] ctx  The context.
+ * @return         True if the resolution was done.
+ *
+ * This function will return true if the resolution has succeeded or failed.
+ * If the resolution has not started or is in progress, return false.
+ */
+bool resolve_thread_done(const resolve_thread_t *ctx);
+
+/**
+ * Query the address.
+ * @param[in] ctx    The context.
+ * @param[out] addr  Pointer to store the address.
+ * @return           True if succeeded.
+ *
+ * If the resolution has succeeded, the resolved address will be stored to `*addr` and will return `true`.
+ * Otherwise, `false` will be returned and `*addr` won't be updated.
+ */
+bool resolve_thread_get_addr(const resolve_thread_t *ctx, struct in_addr *addr);
 
 #ifdef __cplusplus
 } // extern "C"
